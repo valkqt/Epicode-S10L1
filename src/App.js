b347f3d0808470ac378ecd598039b304300196b6
Valk <bznand@gmail.com>
2024-01-09 15:08:26 +0100

    aggiunti spinners e migliorie a post

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React from "react";
 import data from "./data/books.json";
 import NavComp from "./components/NavComp.jsx";
 import Jumbotron from "./components/Jumbotron.jsx";
-
 import SidebarComp from "./components/SidebarComp.jsx";
 import CardComp from "./components/CardComp.jsx";
 import { Container } from "react-bootstrap";
@@ -13,6 +12,9 @@ function App() {
   const [searchText, setSearchText] = React.useState("");
   const [comments, setComments] = React.useState([]);
   const [currentAsin, setcurrentAsin] = React.useState("");
+  const [commentText, setCommentText] = React.useState("");
+  const [rating, setRating] = React.useState("");
+  const [loading, setLoading] = React.useState(false)
 
   const genre = window.location.pathname.split("/").pop();
   const selectedBooks = data[genre || "fantasy"].filter((book) =>
@@ -21,6 +23,8 @@ function App() {
 
   function getComments(asin) {
     const endpoint = `https://striveschool-api.herokuapp.com/api/books/${asin}/comments`;
+
+    setLoading(true)
 
     fetch(endpoint, {
       method: "GET",
@@ -34,7 +38,28 @@ function App() {
       .then((data) => {
         console.log(data);
         setComments(data);
+        setLoading(false)
       });
+  }
+
+  function addComment() {
+    const newPost = {
+      comment: commentText,
+      rate: rating,
+      elementId: `${currentAsin}`,
+    };
+
+    fetch("https://striveschool-api.herokuapp.com/api/comments/", {
+      method: "POST",
+      headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTg0NDZkY2I1MjViYjAwMThlZDA4MTAiLCJpYXQiOjE3MDQ3ODgyNzAsImV4cCI6MTcwNTk5Nzg3MH0.sIGkHZ6yOPihWNtqG6mC3s02nc5O-HTZcpnI45Dpj1I",
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(newPost),
+    })
+      .then((response) => response.json())
+      .then((data) => setComments([...comments, data]));
   }
 
   return (
@@ -66,7 +91,14 @@ function App() {
           })}
         </div>
         <div className="commentsSidebar">
-          <SidebarComp comments={comments} asin={currentAsin} />
+          <SidebarComp
+            comments={comments}
+            asin={currentAsin}
+            onFormSubmit={addComment}
+            textInput={setCommentText}
+            ratingInput={setRating}
+            loading={loading}
+          />
         </div>
       </main>
     </>
--- src/components/SidebarComp.jsx
+++ src/components/SidebarComp.jsx
@@ -1,33 +1,15 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import Spinner from "react-bootstrap/Spinner";
 
 function SidebarComp(props) {
-  const [comment, setComment] = React.useState("");
-  const [rating, setRating] = React.useState("");
-
-  function addComment() {
-    fetch("https://striveschool-api.herokuapp.com/api/comments/", {
-      method: "POST",
-      headers: {
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTg0NDZkY2I1MjViYjAwMThlZDA4MTAiLCJpYXQiOjE3MDQ3ODgyNzAsImV4cCI6MTcwNTk5Nzg3MH0.sIGkHZ6yOPihWNtqG6mC3s02nc5O-HTZcpnI45Dpj1I",
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify({
-        comment: comment,
-        rate: rating,
-        elementId: `${props.asin}`,
-      }),
-    });
-  }
-
   return (
     <>
       <Form
         onSubmit={(e) => {
           e.preventDefault();
-          addComment();
+          props.onFormSubmit();
           e.target.reset();
         }}
       >
@@ -36,7 +18,7 @@ function SidebarComp(props) {
           <Form.Control
             className="my-3"
             placeholder="write a comment"
-            onChange={(e) => setComment(e.target.value)}
+            onChange={(e) => props.textInput(e.target.value)}
           ></Form.Control>
         </Form.Group>
         <Form.Group className="d-flex justify-content-around">
@@ -45,35 +27,35 @@ function SidebarComp(props) {
             label={"1"}
             value={"1"}
             name={"rating"}
-            onClick={(e) => setRating(e.target.value)}
+            onClick={(e) => props.ratingInput(e.target.value)}
           />
           <Form.Check
             type="radio"
             label={"2"}
             value={"2"}
             name={"rating"}
-            onClick={(e) => setRating(e.target.value)}
+            onClick={(e) => props.ratingInput(e.target.value)}
           />
           <Form.Check
             type="radio"
             label={"3"}
             value={"3"}
             name={"rating"}
-            onClick={(e) => setRating(e.target.value)}
+            onClick={(e) => props.ratingInput(e.target.value)}
           />
           <Form.Check
             type="radio"
             label={"4"}
             value={"4"}
             name={"rating"}
-            onClick={(e) => setRating(e.target.value)}
+            onClick={(e) => props.ratingInput(e.target.value)}
           />
           <Form.Check
             type="radio"
             label={"5"}
             value={"5"}
             name={"rating"}
-            onClick={(e) => setRating(e.target.value)}
+            onClick={(e) => props.ratingInput(e.target.value)}
           />
         </Form.Group>
         <Button variant="danger" type="submit">
@@ -82,16 +64,20 @@ function SidebarComp(props) {
       </Form>
       <hr />
       <div>
-        {props.comments.map((comment) => {
-          return (
-            props && (
-              <div key={comment._id} className="d-flex justify-content-between">
-                <p>{comment.comment}</p>
-                <p>{comment.rate}</p>
-              </div>
-            )
-          );
-        })}
+        {props &&
+          props.comments.map((comment) => {
+            return (
+              <Fragment key={comment._id}>
+                {props.loading && <Spinner variant="danger d-block"/>}
+                {!props.loading && (
+                  <div className="d-flex justify-content-between">
+                    <p>{comment.comment}</p>
+                    <p>{comment.rate}</p>
+                  </div>
+                )}
+              </Fragment>
+            );
+          })}
       </div>
     </>
   );
